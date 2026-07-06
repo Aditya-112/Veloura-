5073e9aebbf1ee977bea05552d6808ec061d59cd
Aditya <adityajareda4001@gmail.com>
2026-07-06 17:14:52 +0530

    Initial backend setup with Express and MongoDB

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+//TEST ROUTE 
+app.get("/", (req, res) => {
+  res.send(" Veloura Backend is Running!");
+});
+
+export default app;
